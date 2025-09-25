7806a2a0a7a5abade9bea1adf20c794adc635122
vaishnavi0077 <vaishgarad007@gmail.com>
2025-09-25 15:52:43 +0530

    util code discussion

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# chai or code
+This is  video seriesw
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+dotenv.config({
+    path:'./.env'
+})
+// import express from "express";
+// const app = express()
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("ERRR: ",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//              console.log(`App is listening on port ${process.env.PORT}`);
+//         })"dev": "nodemon src/index.js"
+//     } catch (error) {
+//         console.error("ERROR :" , error)
+//         throw err
+//     }
+    
+// })
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("mongoDB connection fail",err);
+})
